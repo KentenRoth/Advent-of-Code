5b6571dc7cacfaeae83df5d0f8cc0707b0c4241e
Kenten Roth <kentencroth@yahoo.com>
2025-11-25 17:33:27 -0600

    2025 setup C#

---
+++ 2025/Days/Day01.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2025.Days
+{
+    public class Day01 : IDay
+    {
+        public int DayNumber => 1;
+
+        public string Run(string input)
+        {
+            return "Initial Setup";
+        }
+    }
+}
---
+++ 2025/IDay.cs
@@ -0,0 +1,5 @@
+public interface IDay
+{
+    int DayNumber { get; }
+    string Run(string input);
+}
