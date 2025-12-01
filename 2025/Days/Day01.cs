571177783073ad89ae4a7d458e50a46e319d6048
Kenten Roth <kentencroth@yahoo.com>
2025-12-01 17:31:55 -0600

    Part One Answer 980

--- 2025/Days/Day01.cs
+++ 2025/Days/Day01.cs
@@ -11,7 +11,73 @@ namespace _2025.Days
 
         public string Run(string input)
         {
-            return "Initial Setup";
+            string[] lines = input.Split('\n');
+            
+            int currentSpot = 50;
+            int total = 0;
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                string directionToTurn = lines[i].Substring(0, 1);
+                switch (directionToTurn)
+                {
+                    case "L":
+                        TurnLeft(lines[i]);
+                        break;
+                    case "R":
+                        TurnRight(lines[i]);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid direction");
+                        break;
+                }
+            }
+
+            int TurnLeft(string line)
+            {
+                int numberToTurn;
+                    
+                if (line.Length > 2)
+                {
+                    numberToTurn = int.Parse(line.Substring(line.Length -2));
+                    currentSpot -= numberToTurn;
+                }
+                else
+                {
+                    numberToTurn = int.Parse(line.Substring(1));
+                    currentSpot -= numberToTurn;
+                }
+                
+                currentSpot = (currentSpot + 100) % 100;
+
+                if (currentSpot == 0) total++;
+                
+                return currentSpot;
+            }
+            
+            int TurnRight(string line)
+            {
+                int numberToTurn;
+                
+                if (line.Length > 2)
+                {
+                    numberToTurn = int.Parse(line.Substring(line.Length -2));
+                    currentSpot += numberToTurn;
+                }
+                else
+                {
+                    numberToTurn = int.Parse(line.Substring(1));
+                    currentSpot += numberToTurn;
+                }
+
+                currentSpot = (currentSpot + 100) % 100;
+
+                if (currentSpot == 0) total++;
+                
+                return currentSpot;
+            }
+
+            return "Part One " + total.ToString();
         }
     }
 }
